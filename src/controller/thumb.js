27d757a94a371b30bdb59f52d26de21a25edc2cd
Anurag Sharma <anurag.sharma@remotestate.com>
2022-05-24 15:35:24 +0530

    deleting a file

--- src/controller/thumb.js
+++ src/controller/thumb.js
@@ -8,34 +8,83 @@ const thumbnailGenerate = (req, res) => {
     const dateTime = new Date()
 
     const {url, filename} = req.body
-    let TempVideoName =  Math.floor(dateTime.getTime() / 1000)+filename
+    const TempVideoName =  Math.floor(dateTime.getTime() / 1000)+filename
     let file;
     var download = function (url, TempVideoName, callback) {
-        file = fs.createWriteStream(TempVideoName)
-        client.get(url, function (response) {
-            console.log('content-type:', response.headers['content-type']);
-            console.log('content-length:', response.headers['content-length']);
+        try{
+            file = fs.createWriteStream(TempVideoName)
+            client.get(url, function (response) {
+            // console.log('content-type:', response.headers['content-type']);
+            // console.log('content-length:', response.headers['content-length']);
 
             response.pipe(file)
             file.on('finish', callback);
         });
+        }
+        catch(err){
+            res.status(400).json({err:err.message})
+        }
+        
     };
 
     download(url, TempVideoName, async function () {
+        
+        try{
             const ThumbnailFilename = Math.floor(dateTime.getTime() / 1000) + "-thumbnail.png"
 
             const tg = new ThumbnailGenerator({
                 sourcePath: TempVideoName,
-                thumbnailPath: '/home/remotex/Thumbnail/thumbnail',
+                thumbnailPath: 'thumbnail/',
             });
 
             await tg.generate({
                 count: 1,
                 filename: ThumbnailFilename
-            }).then(console.log)
-            const data = fs.readFileSync('/home/remotex/Thumbnail/thumbnail/' + ThumbnailFilename, 'base64')
+            }).catch(console.log)
+            const data = fs.readFileSync('thumbnail/' + ThumbnailFilename)
+            const stat = fs.statSync('thumbnail/' + ThumbnailFilename)    
+            
+    //Deleting the files        
+    function deleteFiles(files, callback){
+        var i = files.length;
+        files.forEach(function(filepath){
+          fs.unlink(filepath, function(err) {
+            i--;
+            if (err) {
+              callback(err);
+              return;
+            } else if (i <= 0) {
+              callback(null);
+            }
+          });
+        });
+      }
+      
+      var files = [TempVideoName,'thumbnail/' + ThumbnailFilename]
+      
+      deleteFiles(files, function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+        //   console.log('all files removed');
+        }
+      });
+            res.set('Content-Type', 'image/jpeg')
+            res.set('Content-Length', stat.size)
+            res.send(data)
+            // res.status(200).sendFile('/home/remotex/thumbnail/thumbnail/' + ThumbnailFilename)
+            // return res
 
-            res.status(200).sendfile('/home/remotex/Thumbnail/thumbnail/' + ThumbnailFilename)
+
+            
+            
+        }
+        catch(err){
+            res.status(500).json({err:err.message})
+        }
+            
+
+            
         }
     )
 
